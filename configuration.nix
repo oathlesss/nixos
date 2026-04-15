ac8c41967942392da310a83f491678054b9a488b
Ruben Hesselink <rubenhesselink@outlook.com>
2026-04-15 19:30:05 +0200

    Version 13
    Fix audio issues

--- configuration.nix
+++ configuration.nix
@@ -40,7 +40,46 @@
       enable = true;
       alsa.enable = true;
       pulse.enable = true;
-      wireplumber.enable = true;
+      wireplumber = {
+        enable = true;
+        extraConfig = {
+          # Stabilize Bluetooth headset behavior (Sony WH-1000XM5 and similar)
+          "10-bluez-properties" = {
+            # Replace the entire BlueZ properties object to prevent codec list merges.
+            "override.monitor.bluez.properties" = {
+              "bluez5.enable-hw-volume" = false;
+              "bluez5.roles" = [ "a2dp_sink" "a2dp_source" "hfp_hf" ];
+              "bluez5.codecs" = [ "sbc" "aac" ];
+            };
+          };
+
+          # Prefer Bluetooth sink when connected and ensure profiles auto-connect
+          "11-bluez-routing" = {
+            "monitor.bluez.rules" = [
+              {
+                matches = [{ "device.name" = "~bluez_card.*"; }];
+                actions."update-props"."bluez5.auto-connect" = [ "a2dp_sink" ];
+              }
+              {
+                matches = [{ "node.name" = "~bluez_output.*"; }];
+                actions."update-props" = {
+                  "priority.driver" = 2000;
+                  "priority.session" = 2000;
+                };
+              }
+            ];
+          };
+
+          # Avoid overly loud default restore levels
+          "20-volume-defaults" = {
+            "wireplumber.settings" = {
+              "device.restore-routes" = true;
+              "node.restore-default-targets" = true;
+              "device.routes.default-sink-volume" = 0.30;
+            };
+          };
+        };
+      };
     };
     dbus.enable = true;
     upower.enable = true;
@@ -81,7 +120,15 @@
     SSH_AUTH_SOCK          = "/run/user/$UID/keyring/ssh";
   };
   time.timeZone = "Europe/Amsterdam";
-  hardware.bluetooth.enable = true;
+  hardware.bluetooth = {
+    enable = true;
+    powerOnBoot = true;
+    settings = {
+      General = {
+        Experimental = true;
+      };
+    };
+  };
 
   system.stateVersion = lib.mkForce "24.11";
 }
